03c17a0f8bba78ce5e809f14367c0312ef495947
Skjalgn <skjalgnysa@hotmail.com>
2023-02-07 17:36:51 +0100

    start2

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("exercies2");
+		config.setTitle("exercise2");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
